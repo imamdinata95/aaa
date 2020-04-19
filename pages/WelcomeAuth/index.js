778b970bdc3ce28b2397e1c19b33746aa5fb977f
imamdinata95 <muhammadimamdinata@gmail.com>
2020-04-19 13:41:40 +0700

    first commit

---
+++ pages/WelcomeAuth/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const WelcomeAuth = () => {
+    return(
+        <View>
+            <Text>Welcome Auth Page</Text>
+        </View>
+
+    );
+};
+
+export default WelcomeAuth;
